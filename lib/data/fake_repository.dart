3d81b1890edf5a0238090021756e006f434b661e
AbdelrahmanElghoul <aelghoul0@gmail.com>
2022-12-03 18:04:46 +0200

    adding assertaion

--- lib/data/fake_repository.dart
+++ lib/data/fake_repository.dart
@@ -9,10 +9,14 @@ import 'package:test_flutter/data/repo_interface.dart';
 class FakeRepository implements IRepository {
   static const String fakeEmail = 'test0@test.com';
   static const String fakePassword = 'Ab123456';
+
   FakeRepository._();
+
   static FakeRepository instance = FakeRepository._();
+
   @override
   Future login({required String? email, required String? password}) async {
+    assert(email != null && password != null);
     await Future.delayed(const Duration(seconds: 5));
 
     if (email == fakeEmail && password == fakePassword) {
